56598bc5752ce7ad77dcc336ea62c610eebd3880
Samuel okoye <samuelokoye92@gmail.com>
2024-02-29 22:22:15 +0100

    added ui  components

---
+++ mern-food-ordering-app/frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,26 @@
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+  if (!domain || !clientId || redirectUri) {
+    throw new Error("unable to initialise auth");
+  }
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: redirectUri }}
+    >
+        {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithNavigate;
