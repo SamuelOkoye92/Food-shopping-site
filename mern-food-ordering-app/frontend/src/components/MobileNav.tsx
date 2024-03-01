e6b6bc9fe0b1c8934c2909a36f6c76f22e9c582a
Samuel okoye <samuelokoye92@gmail.com>
2024-03-01 22:32:23 +0100

    added mobile component

--- mern-food-ordering-app/frontend/src/components/MobileNav.tsx
+++ mern-food-ordering-app/frontend/src/components/MobileNav.tsx
@@ -1,25 +1,46 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { CircleUserRound, Menu } from "lucide-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
-    return(
-        <Sheet>
-            <SheetTrigger>
-                <Menu className="text-orange-500" />
-            </SheetTrigger>
-            <SheetContent className="space-y-3">
-                <SheetTitle>
-                    <span>Welcome to RoyalMeal.com!</span>
-                </SheetTitle>
-                <Separator />
-                <SheetDescription className="flex">
-                    <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
-                </SheetDescription>
-            </SheetContent>
-        </Sheet>
-    );
-}
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span>Welcome to RoyalMeal.com!</span>
+          )}
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+          )}
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default MobileNav;
